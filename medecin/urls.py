458c341660f7d0f34d7d7d954a5ba4948475a888
MaotyKevin <maotykevin@gmail.com>
2024-04-13 18:03:23 +0300

    rectification medecin url

--- medecin/urls.py
+++ medecin/urls.py
@@ -6,6 +6,6 @@ from .views import MedecinListCreateView , MedecinRetrieveUpdateDestroyView
 urlpatterns = [
   
     path('medecin/', MedecinListCreateView.as_view(), name='medecin-list-create'),
-    path('medecin/<int:str>/', MedecinRetrieveUpdateDestroyView.as_view(), name='medecin-detail'),
+    path('medecin/<str:pk>/', MedecinRetrieveUpdateDestroyView.as_view(), name='medecin-detail'),
 
 ]
